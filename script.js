c07c145a47117919431b65f52e077d5504f440ea
NIKITH KUMAR N <nikithkumarniki160698@gmail.com>
2024-04-10 20:17:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function stringChop(str, size) {
   // your code here
+	if(!str){
+		return [];
+	}
+	let chunks = [];
+	for (let i = 0; i <= str.length; i+= size) {
+		const chunk = str.slice(i, i+size);
+		chunks.push(chunk);
+	}
+	return chunks;
 }
 
 // Do not change the code below
